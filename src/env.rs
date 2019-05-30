2b743028145f274dabe884277681c1d437444e8e
MOZGIII <mike-n@narod.ru>
2019-05-31 02:20:18 +0300

    Added env resolution tests

--- src/env.rs
+++ src/env.rs
@@ -16,3 +16,48 @@ pub fn config_from_env() -> Result<Config> {
     let format: LogFormat = log_format_from_env("LOG_FORMAT", LogFormat::Terminal)?;
     Ok(Config { format })
 }
+
+#[test]
+fn env_test_term() {
+    std::env::set_var("LOG_FORMAT", "term");
+    assert_eq!(
+        config_from_env().unwrap(),
+        Config {
+            format: LogFormat::Terminal
+        }
+    );
+}
+
+#[test]
+fn env_test_json() {
+    std::env::set_var("LOG_FORMAT", "json");
+    assert_eq!(
+        config_from_env().unwrap(),
+        Config {
+            format: LogFormat::Json
+        }
+    );
+}
+
+#[test]
+fn env_test_unset() {
+    std::env::remove_var("LOG_FORMAT");
+    assert_eq!(
+        config_from_env().unwrap(),
+        Config {
+            format: LogFormat::Terminal
+        }
+    );
+}
+
+#[test]
+fn env_test_empty() {
+    std::env::set_var("LOG_FORMAT", "");
+    assert!(config_from_env().is_err());
+}
+
+#[test]
+fn env_test_invalid() {
+    std::env::set_var("LOG_FORMAT", "invalid");
+    assert!(config_from_env().is_err());
+}
